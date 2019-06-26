b8df81b7981934262c4b53813ec285150500df16
ancolombo <ancolombo@technogym.com>
2019-06-26 12:01:56 +0200

    Improve resources, add seek bar and meaninful buttons, progress can only go forward though

--- app/src/main/java/eu/acolombo/animatedprogressindicator/MainActivity.kt
+++ app/src/main/java/eu/acolombo/animatedprogressindicator/MainActivity.kt
@@ -1,10 +1,14 @@
 package eu.acolombo.animatedprogressindicator
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.rd.animation.type.AnimationType
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,42 +17,78 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        pageIndicatorView.setAnimationType(AnimationType.THIN_WORM)
+        progressIndicatorView.setAnimationType(AnimationType.THIN_WORM)
+        progressIndicatorView.count = 5
+        progressIndicatorView.min = 0
+        progressIndicatorView.max = 100
 
-        pageIndicatorView.count = 5
+        fun setProgress(progress: Int){
+            val prev = progressIndicatorView.progress
 
-        buttonPlus.setOnClickListener {
-            pageIndicatorView.indicator += 1
+            val animator = ValueAnimator()
+            animator.setObjectValues(prev, progress)
+            animator.addUpdateListener { textProgress.text = "${it.animatedValue}%" }
+            animator.duration = ((maxOf(progress, prev) - minOf(progress, prev)) * 100).toLong()
+            animator.start()
+
+            progressIndicatorView.progress = progress
         }
 
-        buttonMinus.setOnClickListener {
-            pageIndicatorView.indicator -= 1
-        }
-
+        var threadSpeed = 400L
+        var running = false
+        val random = Random(69)
         val mHandler = Handler()
-        var progressPercentage = 0
-        var currentStep = 1
+        val thread = Thread(Runnable {
 
-        Thread(Runnable {
-            while (progressPercentage < 100) {
-                // sleeping for 20milliseconds
+            var p = 0
+
+            while (p < progressIndicatorView.max) {
+
                 try {
-                    Thread.sleep(400)
-                    progressPercentage++
+                    Thread.sleep(threadSpeed)
+                    p++
                 } catch (e: InterruptedException) {
                     e.printStackTrace()
                 }
 
                 mHandler.post {
-                    if (progressPercentage > (pageIndicatorView.max / pageIndicatorView.count - 1) * currentStep) {
-                        currentStep++
-                        pageIndicatorView.indicator++
-                    }
-                    textProgress.text = "$progressPercentage%"
+                    setProgress(p)
                 }
             }
-        }).start()
+        })
+
+        textProgress.setOnClickListener {
+            if (!running) {
+                thread.start()
+                seekBar.visibility = View.VISIBLE
+            } else if (!thread.isInterrupted) {
+                thread.interrupt()
+                running = false
+                seekBar.visibility = View.GONE
+            }
+        }
+
+        buttonMinus.setOnClickListener {
+            setProgress(maxOf(progressIndicatorView.progress - random.nextInt(16), 0))
+        }
+
+        buttonPlus.setOnClickListener {
+            setProgress(minOf(progressIndicatorView.progress + random.nextInt(20), 100))
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                threadSpeed = progress.toLong()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // not implemented
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // not implemented
+            }
+        })
 
     }
-
 }
--- app/src/main/java/eu/acolombo/animatedprogressindicator/ProgressIndicatorView.kt
+++ app/src/main/java/eu/acolombo/animatedprogressindicator/ProgressIndicatorView.kt
@@ -12,63 +12,108 @@ class ProgressIndicatorView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : PageIndicatorView(context, attrs, defStyleAttr) {
 
+//    sealed class State {
+//        object Stop : State()
+//        object Prev : State()
+//        object Next : State()
+//    }
+//
+//    sealed class Event {
+//        object OnProgressValid : Event()
+//        object OnProgressStep : Event()
+//    }
+//
+//    sealed class SideEffect {
+//        object IncrementPosition : SideEffect()
+//        object DecrementPosition : SideEffect()
+//        object IncrementStep : SideEffect()
+//    }
+//
+//    val stateMachine = StateMachine.create<State, Event, SideEffect> {
+//        initialState(State.Stop)
+//        state<State.Stop> {
+//            on<Event.OnProgressValid> {
+//                transitionTo(State.Next, SideEffect.LogMelted)
+//            }
+//        }
+//        state<State.Liquid> {
+//            on<Event.OnFroze> {
+//                transitionTo(State.Solid, SideEffect.LogFrozen)
+//            }
+//            on<Event.OnVaporized> {
+//                transitionTo(State.Gas, SideEffect.LogVaporized)
+//            }
+//        }
+//        state<State.Gas> {
+//            on<Event.OnCondensed> {
+//                transitionTo(State.Liquid, SideEffect.LogCondensed)
+//            }
+//        }
+//        onTransition {
+//            val validTransition = it as? StateMachine.Transition.Valid ?: return@onTransition
+//            when (validTransition.sideEffect) {
+//                SideEffect.LogMelted -> logger.log(ON_MELTED_MESSAGE)
+//                SideEffect.LogFrozen -> logger.log(ON_FROZEN_MESSAGE)
+//                SideEffect.LogVaporized -> logger.log(ON_VAPORIZED_MESSAGE)
+//                SideEffect.LogCondensed -> logger.log(ON_CONDENSED_MESSAGE)
+//            }
+//        }
+//    }
+
+
+
+    var progress = 0
     var min: Int = 0
     var max: Int = 100
+    var state = State.STOP
+    var step = 1
 
-    var mHandler = Handler()
+    enum class State { STOP, NEXT, PREV }
+
+    private val stateMachineHandler = Handler()
+    private var stateMachine: Runnable = object : Runnable {
+
+        var exit = false
+
+        override fun run() {
+
+            val next = (max / (count - 1)) * step
+
+            when (state) {
+                State.STOP -> exitOrExecute {
+                    if (progress > min && progress < max) state = State.NEXT
+                }
+                State.NEXT -> {
+                    selection++
+                    state = if (progress < next) {
+                        State.PREV
+                    } else {
+                        step++
+                        State.STOP
+                    }
+                }
+                State.PREV -> {
+                    selection--
+                    state = State.NEXT
+                }
+            }
+
+            if(!exit) stateMachineHandler.postDelayed(this, animationDuration)
+
+        }
+
+        fun exitOrExecute(execute: () -> Unit) {
+            if (progress >= max) {
+                exit = true
+                stateMachineHandler.removeCallbacks(this)
+            } else {
+                execute()
+            }
+        }
+    }
 
     init {
-        selection = 0
-
-        object : Runnable {
-            override fun run() {
-                // Run the passed runnable
-                animation()
-                // Re-run it after the update interval
-                mHandler.postDelayed(this, animationDuration)
-            }
-        }.run()
+        stateMachine.run()
     }
-
-
-    var indicator = 0
-    var currentStep = 0
-    private var direction = 1
-    private var lastIndicator = 0
-    private var idling: Boolean = true
-
-    private fun animation() {
-        direction = when {
-            direction > 0 -> -1
-            direction < 0 -> 1
-            else -> 0
-        }
-
-        if (idling) {
-            when {
-                direction > 0 -> selection++
-                direction < 0 -> selection--
-            }
-        }
-
-        idling = indicator == lastIndicator
-
-        if (!idling && indicator > lastIndicator ) {
-
-            selection++
-
-            lastIndicator = indicator
-
-
-        } else if (!idling && indicator < lastIndicator && direction < 0) {
-
-            selection--
-
-            lastIndicator = indicator
-
-
-        }
-    }
-
 
 }
